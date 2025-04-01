144595c833500195c5e3b13bc93ce45bcbb01a26
ZavyalovProd <IlyaZav200@gmail.com>
2025-04-01 15:20:37 +0300

    bug_fix4

--- scripts/modal.js
+++ scripts/modal.js
@@ -7,21 +7,21 @@ export function openModal(card, title, description) {
     backgroundBlur.classList.add("backgroundBlur");
     document.body.appendChild(backgroundBlur);
     document.body.appendChild(modal);
-    backgroundBlur.addEventListener("click", closeModal);
-    modal.querySelector(".close_modalWindow_button").addEventListener("click", closeModal);
+    
+    backgroundBlur.addEventListener("click", () => closeModal(modal, backgroundBlur));
+    modal.querySelector(".close_modalWindow_button").addEventListener("click", () => closeModal(modal, backgroundBlur));
 
-    function closeModal() {
-        modal.remove();
-        backgroundBlur.remove();
-      }
-  
     const titleInput = modal.querySelector(".edit_input_title");
     const descriptionInput = modal.querySelector(".edit_input_description");
     modal.querySelector(".modal_form").addEventListener("submit", (event) => {
-      event.preventDefault();
-      title.textContent = titleInput.value;
-      description.textContent = descriptionInput.value;
-      closeModal();
+        event.preventDefault();
+        title.textContent = titleInput.value;
+        description.textContent = descriptionInput.value;
+        closeModal(modal, backgroundBlur);
     });
-  }
-  
+}
+
+export function closeModal(modal, backgroundBlur) {
+    modal.remove();
+    backgroundBlur.remove();
+}
